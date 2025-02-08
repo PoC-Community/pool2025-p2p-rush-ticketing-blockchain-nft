ac0910a37a61e5e297564f640aed53b64b4fca8f
edouardsilvestre <edouard.silvestre@epitech.eu>
2025-02-08 11:27:24 +0100

    [ADD] TicketFactory functions

--- Ticketing_blockchain_NFT/src/Counter.sol
+++ Ticketing_blockchain_NFT/src/Counter.sol
@@ -20,15 +20,41 @@ contract TicketFactory {
         _myTickets.push(myTicket(_defaultLocation, _defaultName, _defaultID, _defaultDate));
     }
 
-    function _createTicket(string memory _name, string memory _location) internal returns (uint256) {
+    function _createTicket() internal returns (uint256) {
         require(_ticketCount[msg.sender] <= 3, "You can't have more than 3 pets");
 
-        myTicket memory newTicket = myTicket({name: _myTickets[0].name, location: _location});
+        myTicket memory newTicket;
+        newTicket.event_name = _myTickets[0].event_name;
+        newTicket.location = _myTickets[0].location;
+
         uint256 newTicketId = _myTickets.length;
         _myTickets.push(newTicket);
         _owners[newTicketId] = msg.sender;
         _ticketCount[msg.sender]++;
 
         return (newTicketId);
+    }
+
+    function getTicketsIdFromAddress(address _owner) public view returns (uint256[] memory) {
+        uint256 count = _ticketCount[_owner];
+        uint256[] memory result = new uint256[](count);
+        uint256 index = 0;
+
+        for (uint256 i = 0; i < _myTickets.length; i++) {
+            if (_owners[i] == _owner) {
+                result[index] = i;
+                index++;
+            }
+        }
+        return result;
+    }
+
+    function getMyTicketsId() public view returns (uint256[] memory) {
+        return getTicketsIdFromAddress(msg.sender);
+    }
+
+    function getMyTickets(uint256 _ticketId) public view returns (myPet memory) {
+        require(_owners[_ticketId] == msg.sender, "You don't own this pet");
+        return _myTickets[_ticketId];
     }
 }
